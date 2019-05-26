98efb795adc896f6c3b467bf82c19c00ab56904d
codypollard <gowercody@gmail.com>
2019-05-26 15:14:35 -0700

    Rewrote depleted command using client.command listener and fixed dict key error with capitalized input

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import discord
+from discord.ext import commands
 import asyncio
 
-client = discord.Client()
+# Client info
+prefix = '!'
+client = commands.Bot(command_prefix=prefix)
 
 time = {'ice':60*60*4, 'eno':60*60*4, 'col':60*60*5}
 name = {'ice':'Ice', 'eno':'Eno', 'col':'Col'}
+
 
 @client.event
 async def on_ready():
     print('Discord bot online as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('!'):
-        task, dep, px = message.content[1:].split(' ')
-        if dep == 'depleted':
-            await wait_and_send(message.channel, task, px)
-        
+@client.command()
+async def depleted(ctx):
+    task = ctx.message.content.split(' ')[1].lower()
+    px = ctx.message.content.split(' ')[2]
+    await wait_and_send(ctx.message.channel, task, px)
+
+
 async def wait_and_send(channel, task, px):
     await channel.send(name[task] + ' marked as depleted for ' + str(px))
     await asyncio.sleep(time[task])
     await channel.send('@here ' + name[task] + ' for ' + str(px) + ' has respawned')
 
-client.run('INSERT BOT TOKEN')
+client.run('Token goes here')
 
